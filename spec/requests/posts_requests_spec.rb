3fd57ca3f8bb768b221b353055414c58c6bff344
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-16 10:16:06 +0100

    Fix linters

--- spec/requests/posts_requests_spec.rb
+++ spec/requests/posts_requests_spec.rb
@@ -2,38 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'PostsController', type: :request do
   context 'get index action' do
-      before(:each) do
-        get '/users/1/posts'
-      end
-  
-      it 'returns a 200 status code' do
+    before(:each) do
+      get '/users/1/posts'
+    end
+
+    it 'returns a 200 status code' do
       expect(response).to have_http_status(:ok)
-      end
-  
-      it 'returns a success message' do
+    end
+
+    it 'returns a success message' do
       expect(response.body).to match(/Posts/)
-      end
-  
-      it 'returns a index template' do
+    end
+
+    it 'returns a index template' do
       expect(response).to render_template('index')
-      end
+    end
   end
-  
+
   context 'get show action' do
-      before(:each) do
-        get '/users/1/posts/1'
-      end
-  
-      it 'returns a 200 status code' do
+    before(:each) do
+      get '/users/1/posts/1'
+    end
+
+    it 'returns a 200 status code' do
       expect(response).to have_http_status(:ok)
-      end
-  
-      it 'returns a success message' do
+    end
+
+    it 'returns a success message' do
       expect(response.body).to match(/Post/)
-      end
-  
-      it 'returns a show template' do
+    end
+
+    it 'returns a show template' do
       expect(response).to render_template('show')
-      end
+    end
   end
 end
