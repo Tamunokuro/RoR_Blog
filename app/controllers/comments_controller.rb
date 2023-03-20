0d69e68c3d4818f3956977f35331eec2967cfdba
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-20 11:36:09 +0100

    Add notice after creating posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @post = Post.find(params[:post_id])
     if @comment.save
-      redirect_to post_path(params[:author_id], @post)
+      redirect_to post_path(params[:author_id], @post), notice: "Comment created successfully"
     else
       render :new
     end
