c95c8170a4a677f15256dc1cab89b5d6f2bccb4d
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-23 13:24:17 +0100

    Add styles to forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 Rails.application.routes.draw do
+
+  #devise
   devise_for :users
+  # Defines the root path route ("/")
+  root "users#index"
+
+  #posts
   get 'users/:author_id/posts', to: 'posts#index', as: 'posts'
   get 'users/:author_id/posts/new', to: 'posts#new', as: 'new_post'
   get 'users/:author_id/posts/:post_id', to: 'posts#show', as: 'post'
   post 'users/:author_id/posts', to: 'posts#create', as: 'create_post'
 
+  #comments
   get 'users/:author_id/posts/:post_id/comments', to: 'comments#index', as: 'comments'
   get 'users/:author_id/posts/:post_id/comments/new', to: 'comments#new', as: 'new_comment'
   get 'users/:author_id/posts/:post_id/comments/:comment_id', to: 'posts#show', as: 'comment'
   post 'users/:author_id/posts/:post_id/comments', to: 'comments#create', as: 'create_comment'
 
+  #likes
   post 'users/:author_id/posts/:post_id/likes', to: 'likes#create', as: 'create_like'
 
-
+  #users
   get 'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "users#index"
 end
